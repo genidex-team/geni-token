69c16db3047cfa9ddc519bd10b24f42f785c9fa8
GeniDexDev <genidexdev@genidex.org>
2025-05-14 18:14:51 +0700

    chore: add hardhat-verify plugin for contract verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
+require("@nomicfoundation/hardhat-verify");
+
 const data = require('geni_data');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
-  networks: data.getNetworkConfig()
+  networks: data.getNetworkConfig(),
+  etherscan: data.getEtherscanConfig()
 };
